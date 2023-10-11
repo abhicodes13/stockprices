ce8444aff48192ea7f817ab698364f81f5814a79
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-10-11 17:55:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,7 @@ const urlEnd = '&format=json&outputsize=30';
 
 
 async function getStocks(name){
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'aaf412f001mshb16825a0d5065e6p181d8ajsn4a009ea4951a',
-			'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
-		}
-	};
+	
 	
 	try {
 		const response = await fetch(url + name + urlEnd , options);
